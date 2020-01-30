430795b13c49b6c954d05c7787d0f514056dc284
Carlos Daniel Barros <barros.carlosds@gmail.com>
2020-01-30 09:55:17 -0300

    first commit

---
+++ src/pages/Main.js
@@ -0,0 +1,136 @@
+import React, { useState, useEffect } from "react";
+
+import { StyleSheet, Image, View, Text, TextInput, TouchableOpacity } from "react-native";
+
+import MapView, { Marker, Callout } from "react-native-maps"
+
+import { MaterialIcons } from "@expo/vector-icons";
+
+import {
+    requestPermissionsAsync, getCurrentPositionAsync
+} from "expo-location";
+
+function Main({ navigation }) {
+    const [ currentRegion, setCurrentRegion  ] = useState(null);
+
+    useEffect(() => {
+        async function loadInitialPosition() {
+            const { granted } = await requestPermissionsAsync();
+
+            if (granted) {
+                const { coords } = await getCurrentPositionAsync({
+                    enableHighAccuracy: true,
+                });
+
+                const { latitude, longitude } = coords;
+
+                setCurrentRegion({
+                    latitude,
+                    longitude,
+                    latitudeDelta: 0.04,
+                    longitudeDelta: 0.04,
+                })
+            }
+        };
+
+        loadInitialPosition();
+        
+    }, []);
+
+    if (!currentRegion) {
+        return null;
+    }
+ 
+    return(
+    <>
+        <MapView initialRegion={currentRegion} style={styles.map}>
+            <Marker coordinate={{ latitude:-10.309066, longitude:-48.3157784 }}>
+                <Image style={styles.avatar} source={{uri:'https://avatars0.githubusercontent.com/u/47464256?s=460&v=4'}} />
+
+                <Callout onPress={() => {
+                    navigation.navigate('Profile', {github_username:'carlosd-barros'});
+                }}>
+                    <View style={styles.callout}>
+                        <Text style={styles.devName}>Carlos Daniel</Text>
+                        <Text style={styles.devBio}>Sistemas de infromação.</Text>
+                        <Text style={styles.devThecs}>Python, Django, Docker</Text>
+                    </View>
+                </Callout>
+            </Marker>
+        </MapView>
+
+        <View style={styles.searchForm}>
+            <TextInput
+                style={styles.searchInput}
+                placeholder="Buscar deves por techs..."
+                placeholderTextColor="#999"
+                autoCapitalize="words"
+                autoCorrect={false}
+            />
+
+            <TouchableOpacity onPress={() => {}} style={styles.loadButton}>
+                <MaterialIcons name="my-location" size={20} color="#FFF" />
+            </TouchableOpacity>
+        </View>
+    </>
+    )
+}
+
+const styles = StyleSheet.create({
+    map:{
+        flex: 1,
+    },
+
+    avatar:{
+        width: 54,
+        height: 54,
+        borderRadius: 4,
+        borderWidth: 4,
+        borderColor: '#FFF'
+    },
+
+    callout:{
+        width: 260,
+    },
+
+    devName:{
+        fontWeight: 'bold',
+        fontSize: 16,
+    },
+
+    devBio:{
+        color: '#666',
+        marginTop: 5,
+    },
+    
+    devThecs:{
+        marginTop: 5,
+    },
+
+    searchForm:{
+        position: "absolute",
+        bottom: 20,
+        left: 20,
+        zIndex: 5,
+        flexDirection: 'row',
+    },
+
+    searchInput:{
+        flex: 1,
+        height: 50,
+        backgroundColor: '#FFF',
+        color: '#333',
+        borderRadius: 250,
+        paddingHorizontal: 20,
+        fontSize: 16,
+        shadowColor: '#000',
+        shadowOpacity: 0.2,
+        shadowOffset: {
+            width: 4,
+            height: 4,
+        },
+        // elevation: 2;
+    },
+});
+
+export default Main;
